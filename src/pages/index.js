e3b25f80ae4c330eca2d209e3d6e7cb7b66f5873
zaiinhs <zainalabidinhsc@gmail.com>
2024-05-13 19:03:50 +0700

    add card recent works

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import { HeadTitle } from "@/components/HeadTitle";
 import Hero from "@/components/Hero";
 import { NavbarComponent } from "@/components/Navbar/NavbarList";
+import { RecentWorksViews } from "@/views";
 import { Box, Container } from "@chakra-ui/react";
 
 export default function Home() {
@@ -16,6 +17,8 @@ export default function Home() {
       <Container maxW={"1024px"}>
         <NavbarComponent />
         <Hero />
+
+        <RecentWorksViews />
       </Container>
     </Box>
   );

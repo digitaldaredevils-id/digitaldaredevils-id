df8bd24ebf9d6d03d70eafea4b493952388fcda6
didik2706 <didikhidayat27@gmail.com>
2024-06-09 17:17:59 +0700

    add FAQ data

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -12,6 +12,55 @@ import {
 import { ChevronDownIcon } from "@chakra-ui/icons";
 
 export default function SimpleAccordion() {
+  const data = [
+    {
+      id: 1,
+      title: "Apa itu software house?",
+      description: "Sebuah software house adalah perusahaan yang berfokus pada pengembangan perangkat lunak atau aplikasi komputer. Kami menyediakan layanan pengembangan custom software, aplikasi web, dan mobile untuk berbagai kebutuhan bisnis."
+    },
+    {
+      id: 2,
+      title: "Apa saja layanan yang ditawarkan oleh software house kami?",
+      description: `Kami menawarkan berbagai layanan pengembangan perangkat lunak, termasuk:
+        - Pengembangan Custom Software: Kami dapat membantu Anda mengembangkan perangkat lunak sesuai dengan kebutuhan bisnis Anda, dari aplikasi desktop hingga aplikasi berbasis cloud.
+        - Pengembangan Aplikasi Web: Kami dapat membuat aplikasi web yang interaktif dan responsif menggunakan teknologi-teknologi terbaru.
+        - Pengembangan Aplikasi Mobile: Kami dapat membuat aplikasi mobile untuk platform Android dan iOS.
+      `
+    },
+    {
+      id: 3,
+      title: "Bagaimana proses pengembangan perangkat lunak di software house kami?",
+      description: `Proses pengembangan perangkat lunak kami meliputi langkah-langkah berikut:
+        - Analisis Kebutuhan: Kami akan bekerja sama dengan Anda untuk memahami dan mengidentifikasi kebutuhan bisnis Anda secara mendalam.
+        Perancangan: Kami akan merancang arsitektur, struktur database, dan antarmuka pengguna yang sesuai dengan kebutuhan Anda.
+        - Implementasi: Tim kami akan mengembangkan perangkat lunak menggunakan bahasa pemrograman dan teknologi yang relevan.
+        - Pengujian dan Debugging: Kami akan melakukan pengujian terperinci untuk memastikan kualitas perangkat lunak, serta melakukan debugging jika ditemukan bug atau error.
+        - Deployment dan Maintenance: Setelah perangkat lunak siap, kami akan membantu dalam proses deployment dan menyediakan pemeliharaan rutin untuk memastikan kinerja yang optimal.
+      `
+    },
+    {
+      id: 4,
+      title: "Apakah software house kami menerima pengembangan perangkat lunak skala kecil maupun besar?",
+      description: "Ya, kami menyediakan layanan pengembangan perangkat lunak untuk skala kecil maupun besar. Kami memiliki pengalaman dalam mengembangkan berbagai jenis proyek, mulai dari solusi perangkat lunak sederhana hingga sistem yang kompleks dan skala enterprise."
+    },
+    {
+      id: 5,
+      title: "Apakah software house kami mampu mengembangkan aplikasi untuk platform mobile?",
+      description: "Ya, kami memiliki tim pengembangan yang terampil dalam mengembangkan aplikasi mobile untuk platform Android dan iOS. Kami dapat membantu Anda merancang dan mengembangkan aplikasi mobile yang responsif dan kaya fitur."
+    },
+    {
+      id: 6,
+      title: "Bagaimana dengan keamanan data pada aplikasi yang dikembangkan oleh software house kami?",
+      description: "Keamanan data adalah prioritas kami. Kami mengimplementasikan praktik-praktik terbaik dalam pengembangan perangkat lunak untuk melindungi data pelanggan kami, mulai dari enkripsi data hingga kebijakan akses yang ketat."
+    },
+    {
+      id: 7,
+      title: "Bagaimana cara menghubungi kami untuk mendapatkan layanan pengembangan perangkat lunak?",
+      description: "Anda dapat menghubungi kami melalui halaman Kontak di website kami atau mengirimkan email ke alamat yang tercantum. Tim kami akan dengan senang hati membantu Anda dan memberikan solusi terbaik untuk kebutuhan pengembangan perangkat lunak Anda."
+    },
+  ];
+
+
   return (
     <Flex direction={"column"} mb={"3em"} align={"center"} justify={"center"}>
       <Text fontSize={"larger"} fontWeight={"bold"}>
@@ -19,62 +68,26 @@ export default function SimpleAccordion() {
       </Text>
       <Container>
         <Accordion allowMultiple width="100%" maxW="lg" rounded="lg">
-          <AccordionItem>
-            <AccordionButton
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              p={4}
-            >
-              <Text fontSize="md">What is Chakra UI?</Text>
-              <ChevronDownIcon fontSize="24px" />
-            </AccordionButton>
-            <AccordionPanel pb={4}>
-              <Text color="gray.600">
-                Chakra UI is a simple and modular component library that gives
-                developers the building blocks they need to create web
-                applications.
-              </Text>
-            </AccordionPanel>
-          </AccordionItem>
-          <AccordionItem>
-            <AccordionButton
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              p={4}
-            >
-              <Text fontSize="md">What advantages to use?</Text>
-              <ChevronDownIcon fontSize="24px" />
-            </AccordionButton>
-            <AccordionPanel pb={4}>
-              <Text color="gray.600">
-                Chakra UI offers a variety of advantages including ease of use,
-                accessibility, and customization options. It also provides a
-                comprehensive set of UI components and is fully compatible with
-                React.
-              </Text>
-            </AccordionPanel>
-          </AccordionItem>
-          <AccordionItem>
-            <AccordionButton
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              p={4}
-            >
-              <Text fontSize="md">How to start using Chakra UI?</Text>
-              <ChevronDownIcon fontSize="24px" />
-            </AccordionButton>
-            <AccordionPanel pb={4}>
-              <Text color="gray.600">
-                To get started with Chakra UI, you can install it via npm or
-                yarn, and then import the components you need in your project.
-                The Chakra UI documentation is also a great resource for getting
-                started and learning more about the library.
-              </Text>
-            </AccordionPanel>
-          </AccordionItem>
+          {data.map((d) => {
+            return <>
+              <AccordionItem id={d.id}>
+                <AccordionButton
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  p={4}
+                >
+                  <Text fontSize="md" align={'left'}>{d.title}</Text>
+                  <ChevronDownIcon fontSize="24px" />
+                </AccordionButton>
+                <AccordionPanel pb={4}>
+                  <Text color="gray.600" align={'left'}>
+                    {d.description}
+                  </Text>
+                </AccordionPanel>
+              </AccordionItem>
+            </>
+          })}
         </Accordion>
       </Container>
     </Flex>
